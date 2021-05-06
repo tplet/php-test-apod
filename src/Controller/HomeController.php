0a22404c06ec86ffcf25fe72a46cf5f717a3d2b1
Thibault PLET <thibault.plet@gmail.com>
2021-05-06 16:32:03 +0200

    Part 3 : Protect page with Google auth

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,42 +4,18 @@
 namespace App\Controller;
 
 
-use App\Nasa\ApodBundle\Services\ApodServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
     /**
-     * @var ApodServiceInterface
+     * Homepage
+     *
+     * @return Response
      */
-    protected ApodServiceInterface $apodService;
-
-    /**
-     * HomeController constructor.
-     * @param ApodServiceInterface $apodService
-     */
-    public function __construct(ApodServiceInterface $apodService)
-    {
-        $this->apodService = $apodService;
-    }
-
-
     public function index(): Response
     {
-        // Get most recent picture
-        $picture = $this->apodService->getMostRecentPicture();
-
-        return $this->render('home/index.html.twig', [
-            'media' => $picture,
-        ]);
-    }
-
-    /**
-     * @return ApodServiceInterface
-     */
-    public function getApodService(): ApodServiceInterface
-    {
-        return $this->apodService;
+        return $this->render('home/index.html.twig');
     }
 }
